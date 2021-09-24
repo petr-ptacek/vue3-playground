165444861edf13971a0feb235e11701278d88311
Petr Ptacek <petr.ptacek99@gmail.com>
2021-09-24 09:40:34 +0200

    Create Card and CardsDashboard components

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,33 @@
 <template>
-  <img alt="Vue logo"
-       src="./assets/logo.png" />
+  <CardsDashboard />
 </template>
 
 <script setup
         lang="ts">
 
+import CardsDashboard from '@/components/CardsDashboard.vue';
 </script>
 
 <style>
+*, *::before, *::after {
+  padding: 0;
+  margin: 0;
+  box-sizing: inherit;
+}
+
+html {
+  font-size: 62.5%;
+}
+
+body {
+  box-sizing: border-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/CardsDashboard.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="cards-dashboard">
+    <Card v-for="card of state.cards"
+          :key="card.id">
+      <template #header>
+        <span>Card #{{ card.id }}</span>
+      </template>
+      <p>
+        {{ card.content }}
+      </p>
+    </Card>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Card                from './Card.vue';
+
+export default defineComponent({
+  name: 'cards-dashboard',
+  data() {
+    return {
+      state: {
+        cards: [
+          {
+            id: 1,
+            content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A architecto culpa dicta exercitationem molestias nesciunt non quas quisquam saepe tempora?'
+          },
+          {
+            id: 2,
+            content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A architecto culpa dicta exercitationem molestias nesciunt non quas quisquam saepe tempora?'
+          },
+          {
+            id: 3,
+            content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A architecto culpa dicta exercitationem molestias nesciunt non quas quisquam saepe tempora?'
+          },
+          {
+            id: 4,
+            content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A architecto culpa dicta exercitationem molestias nesciunt non quas quisquam saepe tempora?'
+          },
+          {
+            id: 5,
+            content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A architecto culpa dicta exercitationem molestias nesciunt non quas quisquam saepe tempora?'
+          },
+          {
+            id: 6,
+            content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A architecto culpa dicta exercitationem molestias nesciunt non quas quisquam saepe tempora?'
+          },
+          {
+            id: 7,
+            content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A architecto culpa dicta exercitationem molestias nesciunt non quas quisquam saepe tempora?'
+          },
+          {
+            id: 8,
+            content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A architecto culpa dicta exercitationem molestias nesciunt non quas quisquam saepe tempora?'
+          },
+          {
+            id: 9,
+            content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A architecto culpa dicta exercitationem molestias nesciunt non quas quisquam saepe tempora?'
+          },
+          {
+            id: 10,
+            content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A architecto culpa dicta exercitationem molestias nesciunt non quas quisquam saepe tempora?'
+          },
+          {
+            id: 11,
+            content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A architecto culpa dicta exercitationem molestias nesciunt non quas quisquam saepe tempora?'
+          },
+          {
+            id: 12,
+            content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A architecto culpa dicta exercitationem molestias nesciunt non quas quisquam saepe tempora?'
+          }
+        ]
+      }
+    };
+  },
+  components: {
+    Card
+  }
+});
+</script>
+
+<style scoped>
+.cards-dashboard {
+  padding: 5rem;
+  background-color: orangered;
+  display: grid;
+  grid-gap: 2.5rem;
+  grid-template-columns: repeat(auto-fit, minmax(25rem, 1fr));
+}
+</style>
